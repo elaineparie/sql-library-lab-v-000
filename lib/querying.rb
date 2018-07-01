c0800cd82cb7956339adfcf443f1b85d02e1847b
Elaine Anderson <eparie@bu.edu>
2018-07-01 22:37:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+  JOIN series
+  on books.series_id = series.id
+  WHERE series.id = 1
+  ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto from characters
+  WHERE characters.motto = (SELECT GREATEST(characters.motto))"
 end
 
 
