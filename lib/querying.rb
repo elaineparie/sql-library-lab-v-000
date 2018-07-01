6e197376a63d19327a8d0b0ae1d39ec43f84759f
Elaine Anderson <eparie@bu.edu>
2018-07-01 23:57:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   series.subgenre_id = series.author_id
   JOIN subgenres ON
   subgenres.id = series.subgenre_id
-  GROUP BY series.subgenre_id"
+  GROUP BY series.id"
 end
 
 def select_series_title_with_most_human_characters
