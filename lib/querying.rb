d645827f3e414b1a5f38f32ed97b8e3c110e76a8
Elaine Anderson <eparie@bu.edu>
2018-07-02 00:09:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,6 +37,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.id) FROM characters
-  JOIN characters_books ON
-  characters_books.book_id = characters_books.character_id" 
+  JOIN character_books ON
+  character_books.book_id = character_books.character_id" 
 end
