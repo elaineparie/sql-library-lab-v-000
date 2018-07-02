6ed09fd5d06bebfdc52f6290a1cdfcce5ac32228
Elaine Anderson <eparie@bu.edu>
2018-07-02 00:02:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_series_title_with_most_human_characters
   JOIN characters ON
   series.id = characters.series_id
   GROUP BY characters.species
-  ORDER BY COUNT(characters.species) DESC LIMIT 1"
+  HAVING COUNT(characters.species = 'human') DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
